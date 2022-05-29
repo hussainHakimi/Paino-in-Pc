0fb75a9030fc5da08427a3b50db748f367be40cf
hussainHakimi <hussainbayan614@gmail.com>
2022-05-29 16:06:28 +0430

    javascript code is done.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  
   const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
 
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add('isplaying');
+  this.setTimeout(function() {
+    key.classList.remove('isplaying');
+  },500);
 });
 
-function removeTransition(e){
-if(e.propertyName !== 'transform') return;
-this.classList.remove('isplaying');
-}
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+window.addEventListener('keydown', function(e){
+  console.log(e.keyCode);
+});
